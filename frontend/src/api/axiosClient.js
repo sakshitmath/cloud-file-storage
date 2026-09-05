0c3bd27a0af4abbb06a809ae36d8fa5130aa0cee
Sakshi Torgalmath <sakshitmath@gmail.com>
2026-09-05 13:40:48 +0530

    Use env var for API base URL

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8081/api',
 })
 
 axiosClient.interceptors.request.use((config) => {
